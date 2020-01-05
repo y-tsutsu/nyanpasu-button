114406c6d174502b9f7e57a8f131e7ce166b9bca
tsutsu <tsutsu0928@gmail.com>
2020-01-06 00:40:33 +0900

    デザイン微調整

--- nyanpasu-button/nyanpasu-button.Android/MainActivity.cs
+++ nyanpasu-button/nyanpasu-button.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace nyanpasu_button.Droid
 {
-    [Activity(Label = "nyanpasu_button", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "にゃんぱすーボタン", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
